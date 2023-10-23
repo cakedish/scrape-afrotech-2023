3b6dc0bf34b58b06573e858994f8df083b2da641
cakedish <47048949+cakedish@users.noreply.github.com>
2023-10-23 18:52:22 +0000

    Foiled again by those meddling linters

--- access_types.md
+++ access_types.md
@@ -22,3 +22,42 @@
 <option value="9">RSVP Required</option>
 <option value="10">Add-On</option>
 ```
+
+```python
+# Test the function
+import cleanup_access_types
+
+if __name__ == "__main__":
+    # Example usage:
+    access_types_list = ["Students", "Executive", "Speakers"]
+    lowest_value = this(access_types_list)
+    print("Lowest Value:", lowest_value)
+
+    event_data = [
+        {"access": ["Students", "Executive", "Speakers"]},
+        {"access": ["Students", "Student"]},
+        {"access": ["Speakers", "Speakers & All Access Registration"]},
+        {
+            "access": [
+                "All Access Badge Holders",
+                "All Access",
+                "All Access Ticket Holders",
+            ]
+        },
+        {"access": ["Corporate", "Corporate Sponsors"]},
+        {"access": ["Executive", "Exec"]},
+        {"access": ["RSVP Only", "Invite Only", "RSVP Required", "RSVP Requirfed"]},
+        {"access": ["GA"]},
+        {"access": ["Press"]},
+        {"access": ["Sponsors"]},
+        {"access": ["Add-On for all Others"]},
+    ]
+
+    access_type_strings = {v: k for k, v in access_type_levels.items()}
+
+    # Iterate through the list of event objects and print lowest access level
+    for event in event_data:
+        print("IN : ", event["access"])
+        level = this(cleanup_access_types.this(event["access"]))
+        print(f"OUT: {level} -> {access_type_strings[level]}")
+```
--- determine_lowest_access.py
+++ determine_lowest_access.py
@@ -1,7 +1,6 @@
 """
 Determine the lowest access type value from a list of access types
 """
-import cleanup_access_types
 
 # Define a dictionary to map access type names to their values
 access_type_levels = {
@@ -32,38 +31,3 @@ def this(access_types):
             low = value
 
     return low
-
-
-# Test the function
-if __name__ == "__main__":
-    # Example usage:
-    access_types_list = ["Students", "Executive", "Speakers"]
-    lowest_value = this(access_types_list)
-    print("Lowest Value:", lowest_value)
-
-    event_data = [
-        {"access": ["Students", "Student"]},
-        {"access": ["Speakers", "Speakers & All Access Registration"]},
-        {
-            "access": [
-                "All Access Badge Holders",
-                "All Access",
-                "All Access Ticket Holders",
-            ]
-        },
-        {"access": ["Corporate", "Corporate Sponsors"]},
-        {"access": ["Executive", "Exec"]},
-        {"access": ["RSVP Only", "Invite Only", "RSVP Required", "RSVP Requirfed"]},
-        {"access": ["GA"]},
-        {"access": ["Press"]},
-        {"access": ["Sponsors"]},
-        {"access": ["Add-On for all Others"]},
-    ]
-
-    access_type_strings = {v: k for k, v in access_type_levels.items()}
-
-    # Iterate through the list of event objects and print lowest access level
-    for event in event_data:
-        print("IN : ", event["access"])
-        level = this(cleanup_access_types.this(event["access"]))
-        print(f"OUT: {level} -> {access_type_strings[level]}")
